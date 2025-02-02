266461283993f7abbb60c6b6fa8b3006953d0a2d
akash-65 <akash.ap1283@gmail.com>
2025-02-02 23:46:43 +0530

    Implemented DTO for Category

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.ecommerce.project.controller;
 
-import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryDTO;
+import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -19,28 +18,26 @@ public class CategoryController {
 
 
     @GetMapping("/public/categories")
-    public ResponseEntity<List<Category>> getAllCategories() {
-        List<Category> categories = categoryService.getAllCategories();
-        return new ResponseEntity<>(categories, HttpStatus.OK);
+    public ResponseEntity<CategoryResponse> getAllCategories() {
+        CategoryResponse categoryResponse = categoryService.getAllCategories();
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
-        categoryService.createCategory(category);
-        return new ResponseEntity<>("Categories Added Successfully !!!", HttpStatus.CREATED);
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
+        return new ResponseEntity<>(savedCategoryDTO, HttpStatus.CREATED);
     }
 
     @PutMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<String> updateCategories(@Valid @RequestBody Category category, @PathVariable Long categoryId) {
-        Category savedCategory = categoryService.updateCategory(category, categoryId);
-        return new ResponseEntity<>("Category with ID: " + categoryId + " Update Successfully !!!", HttpStatus.OK);
+    public ResponseEntity<CategoryDTO> updateCategories(@Valid @RequestBody CategoryDTO categoryDTO, @PathVariable Long categoryId) {
+        CategoryDTO savedCategoryDTO = categoryService.updateCategory(categoryDTO, categoryId);
+        return new ResponseEntity<>(savedCategoryDTO, HttpStatus.OK);
     }
 
     @DeleteMapping("/admin/categories/{CategoryId}")
-    public ResponseEntity<String> deleteCategory(@PathVariable Long CategoryId) {
-        String status = categoryService.deleteCategory(CategoryId);
-        return new ResponseEntity<>(status, HttpStatus.OK);
-        // return ResponseEntity.ok(status);
-        // return ResponseEntity.status(HttpStatus.OK).body(status);
+    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long CategoryId) {
+        CategoryDTO deleteCategory = categoryService.deleteCategory(CategoryId);
+        return new ResponseEntity<>(deleteCategory, HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ecommerce.project.service;
 
-import com.ecommerce.project.com.ecommerce.project.repositories.CategoryRepository;
 import com.ecommerce.project.exceptions.APIException;
 import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryDTO;
+import com.ecommerce.project.payload.CategoryResponse;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,38 +18,83 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
-    public List<Category> getAllCategories() {
+    public CategoryResponse getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
+
+        // Validation
         if (categories.isEmpty()) {
             throw new APIException("No Category created till now !!!");
         }
-        return categories;
+
+        // Map every object from Category with the Category DTO class
+        List<CategoryDTO> categoryDTOS = categories.stream()
+                .map(category -> modelMapper.map(category, CategoryDTO.class))
+                .toList();
+
+        // Save the newly created categoryDTOS into the categoryResponse
+        CategoryResponse categoryResponse = new CategoryResponse();
+        categoryResponse.setContent(categoryDTOS);
+
+        return categoryResponse;
     }
 
     @Override
-    public void createCategory(Category category) {
-        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
-        if (savedCategory != null) {
+    public CategoryDTO createCategory(CategoryDTO categoryDTO) {
+
+        // We are getting CategoryDTO from RequestBody but to save it in DB we need to convert it into
+        // Category Entity
+        Category category = modelMapper.map(categoryDTO, Category.class);
+
+        // Validation
+        Category categoryFromDb = categoryRepository.findByCategoryName(category.getCategoryName());
+        if (categoryFromDb != null) {
             throw new APIException("Category with the name " + category.getCategoryName() + " already exists !!!");
         }
-        categoryRepository.save(category);
+
+        // Save
+        Category savedCategory = categoryRepository.save(category);
+
+        // Now we got saved Category Entity but send to user Response we need to convert it into DTO
+        return modelMapper.map(savedCategory, CategoryDTO.class);
     }
 
     @Override
-    public String deleteCategory(Long categoryId) {
-        Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
-        categoryRepository.delete(category);
-        return "Category with categoryID : " + categoryId + " Deleted Successfully !!!";
-    }
+    public CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId) {
 
-    @Override
-    public Category updateCategory(Category category, Long categoryId) {
+        // We are getting CategoryDTO from RequestBody but to save it in DB we need to convert it into
+        // Category Entity
+        Category category = modelMapper.map(categoryDTO, Category.class);
+
+        // Validation
         Category savedCategory = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
         category.setCategoryID(categoryId);
+
+        // Save
+        category.setCategoryID(categoryId);
         savedCategory = categoryRepository.save(category);
-        return savedCategory;
+
+        // Now we got saved Category Entity but send to user Response we need to convert it into DTO
+        return modelMapper.map(savedCategory, CategoryDTO.class);
     }
+
+    @Override
+    public CategoryDTO deleteCategory(Long categoryId) {
+
+        // Validation
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+
+        // Delete
+        categoryRepository.delete(category);
+
+        // Now we got saved Category Entity but send to user Response we need to convert it into DTO
+        return modelMapper.map(category, CategoryDTO.class);
+    }
+
+
 }
